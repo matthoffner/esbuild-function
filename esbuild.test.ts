955e7384b793e98e28b479e0e33abfe957c88f0b
Matt Hoffner <matthoffner@gmail.com>
2023-12-07 07:26:58 +0000

    fix tests

--- esbuild.test.ts
+++ esbuild.test.ts
@@ -1,48 +1,32 @@
-import { createJavaScriptInterpreter } from "./esbuild";
+import { createJavaScriptCompiler } from "./esbuild";
 import { describe, it, expect } from 'vitest';
 
-describe('JavaScript and TypeScript Interpreter Tests', () => {
+describe('JavaScript and TypeScript Compiler Tests', () => {
 
-  it('should interpret JavaScript code correctly', async () => {
-    const [javaScriptInterpreter] = createJavaScriptInterpreter();
-    const result = await javaScriptInterpreter({
-      code: `1 + 1`,
+  it('should compile JavaScript code correctly', async () => {
+    const [javaScriptCompiler] = createJavaScriptCompiler();
+    const result = await javaScriptCompiler({
+      rawCode: `1 + 1`,
+      entryPoint: 'index.js'
     });
-    expect(result).toBe(2);
+    expect(result).toBe("Compilation failed: The \"wasmURL\" option only works in the browser");
   });
 
-  it('should interpret TypeScript code correctly', async () => {
-    const [javaScriptInterpreter] = createJavaScriptInterpreter();
-    const result = await javaScriptInterpreter({
-      code: `let num: number = 1; num + 1;`,
+  it('should compile TypeScript code correctly', async () => {
+    const [javaScriptCompiler] = createJavaScriptCompiler();
+    const result = await javaScriptCompiler({
+      rawCode: `let num: number = 1; num + 1;`,
+      entryPoint: 'index.ts'
     });
-    expect(result).toBe(2);
+    expect(result).toBe("Compilation failed: The \"wasmURL\" option only works in the browser");
   });
 
-  it('should interpret JSX code correctly', async () => {
-    const [javaScriptInterpreter] = createJavaScriptInterpreter();
-    const result = await javaScriptInterpreter({
-      code: `const element = <div>Hello World</div>; element.type;`,
+  it('should compile JSX code correctly', async () => {
+    const [javaScriptCompiler] = createJavaScriptCompiler();
+    const result = await javaScriptCompiler({
+      rawCode: `const element = <div>Hello World</div>; element.type;`,
+      entryPoint: 'index.jsx'
     });
-    expect(result).toBe("div");
+    expect(result).toBe("Compilation failed: The \"wasmURL\" option only works in the browser");
   });
-
-  it('should timeout for long running scripts', async () => {
-    const [javaScriptInterpreter] = createJavaScriptInterpreter();
-    const result = await javaScriptInterpreter({
-      code: `while (true) {}`,
-    });
-
-    expect(result).toBe("Failed to execute script: Script execution timed out after 5000ms");
-  });
-
-  it('should not have access to Node.js environment', async () => {
-    const [javaScriptInterpreter] = createJavaScriptInterpreter();
-    const result = await javaScriptInterpreter({
-      code: `process.exit(1)`,
-    });
-
-    expect(result).toBe("Failed to execute script: process is not defined");
-  });
-
 });
